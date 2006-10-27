d442cc9fbdf45a28fe02f13552f44e3e2f7ec22e
Jonathan Rockway <jon@jrock.us>
2006-10-27 16:53:28 +0000

    Moving tutorial POD here

---
+++ lib/Catalyst/Manual.pm
@@ -0,0 +1,25 @@
+#!perl
+# Manual.pm 
+# Copyright (c) 2006 Jonathan Rockway <jrockway@cpan.org>
+
+package Catalyst::Manual;
+
+=head1 NAME
+
+Catalyst::Manual - pacakge holding the Catalyst developer's manual and tutorial
+
+=head1 SYNOPSIS
+
+    perldoc Catalyst::Manual::Intro
+    perldoc Catalyst::Tutorial
+
+=head1 SEE ALSO
+
+Get L<Task::Catalyst::Tutorial|Task::Catalyst::Tutorial> to install
+all the dependencies you need to follow along with the Tutorial.
+
+=cut
+
+our $VERSION = '0.01';
+
+1;
